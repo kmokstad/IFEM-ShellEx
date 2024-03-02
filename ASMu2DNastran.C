6439b40e2309cba42ac30d8bbbe9f71470411bad
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2024-03-02 18:17:40 +0100

    Add main program and classes ASMu2DNastran and SIMAndesShell

---
+++ ASMu2DNastran.C
@@ -0,0 +1,181 @@
+// $Id$
+//==============================================================================
+//!
+//! \file ASMu2DMastran.C
+//!
+//! \date Feb 27 2024
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Assembly of unstructured 2D Lagrange FE models from Nastran BDF-file.
+//!
+//==============================================================================
+
+#include "ASMu2DNastran.h"
+#ifdef HAS_FFLLIB
+#include "FFlLinkHandler.H"
+#include "FFlNastranReader.H"
+#include "FFlElementBase.H"
+#include "FFlFEParts/FFlNode.H"
+#include "FFlFEParts/FFlPMAT.H"
+#include "FFlFEParts/FFlPTHICK.H"
+
+
+class MyNastranReader : public FFlNastranReader
+{
+public:
+  MyNastranReader(FFlLinkHandler& fePart, int lCount)
+    : FFlNastranReader(&fePart,lCount) {}
+  virtual ~MyNastranReader() {};
+
+  bool readAndResolve(std::istream& is)
+  {
+    if (!this->resolve(this->read(is)))
+      myLink->deleteGeometry(); // Parsing failure, delete all FE data
+    else if (nWarnings+nNotes > 0)
+      std::cout <<"\n  ** Parsing FE data succeeded."
+                <<"\n     However, "<< nWarnings
+                <<" warning(s) and "<< nNotes <<" note(s) were reported.\n"
+                <<"     Review the messages and check the FE data file.\n";
+    return myLink->hasGeometry();
+  }
+};
+#endif
+
+
+bool ASMu2DNastran::read (std::istream& is)
+{
+  myMLGE.clear();
+  myMLGN.clear();
+  myCoord.clear();
+
+  // Fast-forward until "BEGIN BULK"
+  int lCount = 0;
+  char cline[256];
+  while (is.getline(cline,255))
+    if (strncmp(cline,"BEGIN BULK",10))
+      ++lCount;
+    else
+      break;
+
+  if (!is) return false; // No bulk data file
+
+#ifdef HAS_FFLLIB
+  FFlLinkHandler  fem;
+  MyNastranReader reader(fem,lCount);
+  if (!reader.readAndResolve(is) || !fem.resolve())
+  {
+    std::cout <<"\n *** Parsing/resolving FE data failed.\n"
+              <<"     The FE model is probably not consistent and has not been"
+              <<" resolved completely.\n";
+    return false;
+  }
+
+  nnod = fem.getNodeCount();
+  nel  = fem.getElementCount(FFlTypeInfoSpec::SHELL_ELM);
+#ifdef INT_DEBUG
+  fem.dump();
+#else
+  std::cout <<"\nTotal number of nodes:          "<< nnod
+            <<"\nNumber of shell elements:       "<< nel
+            <<"\nNumber of constraint elements:  "
+            << fem.getElementCount(FFlTypeInfoSpec::CONSTRAINT_ELM)
+            <<"\nNumber of other elements:       "
+            << fem.getElementCount(FFlTypeInfoSpec::OTHER_ELM)
+            << std::endl;
+#endif
+
+  myMLGN.reserve(nnod);
+  myMLGE.reserve(nel);
+  myCoord.reserve(nnod);
+
+  for (NodesCIter n = fem.nodesBegin(); n != fem.nodesEnd(); ++n)
+  {
+    int nid = (*n)->getID();
+    const FaVec3& X = (*n)->getPos();
+#if INT_DEBUG > 1
+    std::cout << myMLGN.size() <<" "<< nid <<": "<< X << std::endl;
+#endif
+    myMLGN.push_back(nid);
+    myCoord.push_back(Vec3(X.x(),X.y(),X.z()));
+    if ((*n)->isExternal())
+      this->getNodeSet("ASET",lCount).push_back(myMLGN.size());
+  }
+
+  for (ElementsCIter e = fem.elementsBegin(); e != fem.elementsEnd(); ++e)
+  {
+    int eid = (*e)->getID();
+    IntVec mnpc((*e)->getNodeCount());
+    for (size_t i = 0; i < mnpc.size(); i++)
+      mnpc[i] = this->getNodeIndex((*e)->getNodeID(1+i)) - 1;
+    if ((*e)->getCathegory() == FFlTypeInfoSpec::SHELL_ELM)
+    {
+#if INT_DEBUG > 1
+      std::cout <<"Shell element "<< myMLGE.size() <<" "<< eid <<":";
+      for (int node : mnpc) std::cout <<" "<< MLGN[node];
+#endif
+      myMLGE.push_back(eid);
+      myMNPC.push_back(mnpc);
+
+      ShellProps& sprop = myProps[eid];
+      FFlPMAT* mat = dynamic_cast<FFlPMAT*>((*e)->getAttribute("PMAT"));
+      if (mat)
+      {
+        sprop.Emod = mat->youngsModule.getValue();
+        sprop.Rny  = mat->poissonsRatio.getValue();
+        sprop.Rho  = mat->materialDensity.getValue();
+      }
+      else
+        std::cout <<"  ** No material attached to element "<< eid
+                  <<", using default properties"<< std::endl;
+
+      FFlPTHICK* thk = dynamic_cast<FFlPTHICK*>((*e)->getAttribute("PTHICK"));
+      if (thk)
+        sprop.Thick = thk->thickness.getValue();
+      else
+        std::cout <<"  ** No shell thickness attached to element "<< eid
+                  <<", using default value "<< sprop.Thick << std::endl;
+
+#if INT_DEBUG > 1
+      std::cout << sprop << std::endl;
+#endif
+    }
+    else if ((*e)->getTypeName() == "RGD" && mnpc.size() > 1)
+    {
+#if INT_DEBUG > 1
+      std::cout <<"Rigid element "<< eid <<": master = "<< MLGN[mnpc.front()]
+                <<" slaves =";
+      for (size_t i = 1; i < mnpc.size(); i++) std::cout <<" "<< MLGN[mnpc[i]];
+      std::cout << std::endl;
+#endif
+      for (int& n : mnpc) ++n; // Need 1-based node indices
+      this->addRigidCouplings((*e)->getNodeID(1),myCoord[mnpc.front()-1],
+                              IntVec(mnpc.begin()+1,mnpc.end()));
+    }
+    else
+      std::cout <<"  ** Ignored element "<< (*e)->getTypeName()
+                <<" "<< eid << std::endl;
+  }
+#endif
+
+  return true;
+}
+
+
+bool ASMu2DNastran::getProps (int eId, double& E, double& nu,
+                              double& rho, double& t) const
+{
+  std::map<int,ShellProps>::const_iterator it = myProps.find(eId);
+  if (it == myProps.end())
+  {
+    std::cerr <<" *** No properties for shell element "<< eId << std::endl;
+    return false;
+  }
+
+  E   = it->second.Emod;
+  nu  = it->second.Rny;
+  rho = it->second.Rho;
+  t   = it->second.Thick;
+
+  return true;
+}
---
+++ ASMu2DNastran.h
@@ -0,0 +1,63 @@
+// $Id$
+//==============================================================================
+//!
+//! \file ASMu2DMastran.h
+//!
+//! \date Feb 27 2024
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Assembly of unstructured 2D Lagrange FE models from Nastran BDF-file.
+//!
+//==============================================================================
+
+#ifndef _ASM_U2D_NASTRAN_H
+#define _ASM_U2D_NASTRAN_H
+
+#include "ASMu2DLag.h"
+
+
+/*!
+  \brief Driver for assembly of unstructured 2D Lagrange FE models.
+  \details This class overrides the read() method of its parent class
+  assuming the mesh data is stored in the Nastran Bulk Data Format.
+*/
+
+class ASMu2DNastran : public ASMu2DLag
+{
+public:
+  //! \brief The constructor forwards to the parent class constructor.
+  ASMu2DNastran(unsigned char n, unsigned char n_f, char fType = 'N')
+    : ASMu2DLag(n,n_f,fType) {}
+  //! \brief Disable default copy constructor.
+  ASMu2DNastran(const ASMu2DNastran& pch) = delete;
+  //! \brief Empty destructor.
+  virtual ~ASMu2DNastran() {}
+
+  //! \brief Creates an instance by reading the given input stream.
+  virtual bool read(std::istream& is);
+
+  //! \brief Retrieves the properties for element with index \a id.
+  bool getProps(int eid, double& E, double& nu, double& rho, double& t) const;
+
+private:
+  //! \brief Data type for shell element properties.
+  struct ShellProps
+  {
+    double Thick = 0.1;    //!< Shell thickness
+    double Emod  = 2.1e11; //!< Young's modulus
+    double Rny   = 0.3;    //!< Poisson's ratio
+    double Rho   = 7850.0; //!< Mass density
+  };
+
+  //! \brief Output stream operator.
+  friend std::ostream& operator<<(std::ostream& os, const ShellProps& p)
+  {
+    return os <<" t="<< p.Thick <<" E="<< p.Emod <<" nu="<< p.Rny
+              <<" rho="<< p.Rho;
+  }
+
+  std::map<int,ShellProps> myProps; //!< Shell element property container
+};
+
+#endif
