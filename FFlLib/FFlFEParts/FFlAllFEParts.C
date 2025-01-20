kmokstad/fedem-foundation

Most of the attribute classes don't have references to other attributes, so remove the FFlFEAttributeSpec singelton object for those types

src/FFlLib/FFlFEParts/FFlAllFEParts.C
@@ -108,10 +108,15 @@ template<class T> void releaseAttribute()
 }
 
 
+template<class T> void releaseTypeInfo()
+{
+  FFaSingelton<FFlTypeInfoSpec,T>::removeInstance();
+}
+
+
 void FFl::releaseAllElements()
 {
-  FFaSingelton<FFlTypeInfoSpec,FFlNode>::removeInstance();
-
+  releaseTypeInfo<FFlNode>();
   releaseElement<FFlRGD>();
   releaseElement<FFlRBAR>();
   releaseElement<FFlWAVGM>();
@@ -145,43 +150,43 @@ void FFl::releaseAllElements()
   releaseAttribute<FFlSURFLOAD>();
   LoadFactory::removeInstance();
 
-  releaseAttribute<FFlPMASS>();
-  releaseAttribute<FFlPSPRING>();
-  releaseAttribute<FFlPBUSHCOEFF>();
-  releaseAttribute<FFlPBUSHECCENT>();
-  releaseAttribute<FFlPORIENT>();
-  releaseAttribute<FFlPORIENT3>();
-  releaseAttribute<FFlPBEAMECCENT>();
-  releaseAttribute<FFlPBEAMSECTION>();
-  releaseAttribute<FFlPBEAMPIN>();
-  releaseAttribute<FFlPEFFLENGTH>();
-  releaseAttribute<FFlPSPRING>();
-  releaseAttribute<FFlPTHICK>();
-  releaseAttribute<FFlPCOMP>();
-  releaseAttribute<FFlPNSM>();
-  releaseAttribute<FFlPRGD>();
-  releaseAttribute<FFlPWAVGM>();
-  releaseAttribute<FFlPRBAR>();
-  releaseAttribute<FFlPMAT>();
-  releaseAttribute<FFlPMAT2D>();
-  releaseAttribute<FFlPMAT3D>();
-  releaseAttribute<FFlPMATSHELL>();
-  releaseAttribute<FFlPCOORDSYS>();
+  releaseTypeInfo<FFlPMASS>();
+  releaseTypeInfo<FFlPSPRING>();
+  releaseTypeInfo<FFlPBUSHCOEFF>();
+  releaseTypeInfo<FFlPBUSHECCENT>();
+  releaseTypeInfo<FFlPORIENT>();
+  releaseTypeInfo<FFlPORIENT3>();
+  releaseTypeInfo<FFlPBEAMECCENT>();
+  releaseTypeInfo<FFlPBEAMSECTION>();
+  releaseTypeInfo<FFlPBEAMPIN>();
+  releaseTypeInfo<FFlPEFFLENGTH>();
+  releaseTypeInfo<FFlPSPRING>();
+  releaseTypeInfo<FFlPTHICK>();
+  releaseTypeInfo<FFlPCOMP>();
+  releaseTypeInfo<FFlPNSM>();
+  releaseTypeInfo<FFlPRGD>();
+  releaseTypeInfo<FFlPWAVGM>();
+  releaseTypeInfo<FFlPRBAR>();
+  releaseTypeInfo<FFlPMAT>();
+  releaseTypeInfo<FFlPMAT2D>();
+  releaseTypeInfo<FFlPMAT3D>();
+  releaseTypeInfo<FFlPMATSHELL>();
+  releaseTypeInfo<FFlPCOORDSYS>();
 #ifdef FT_USE_STRAINCOAT
   releaseAttribute<FFlPSTRC>();
-  releaseAttribute<FFlPHEIGHT>();
   releaseAttribute<FFlPTHICKREF>();
-  releaseAttribute<FFlPFATIGUE>();
+  releaseTypeInfo<FFlPHEIGHT>();
+  releaseTypeInfo<FFlPFATIGUE>();
 #endif
   AttributeFactory::removeInstance();
 
 #ifdef FT_USE_VISUALS
-  FFlVAppearanceTypeInfoSpec::removeInstance();
-  FFlVDetailTypeInfoSpec::removeInstance();
+  releaseTypeInfo<FFlVAppearance>();
+  releaseTypeInfo<FFlVDetailType>();
   VisualFactory::removeInstance();
 #endif
 
-  FFaSingelton<FFlTypeInfoSpec,FFlGroup>::removeInstance();
+  releaseTypeInfo<FFlGroup>();
 
   FFlMemPool::deleteAllLinkMemPools();
 

kmokstad/IFEM-ShellEx

FFlLib/FFlFEParts/FFlAllFEParts.C
@@ -108,10 +108,15 @@ template<class T> void releaseAttribute()
 }
 
 
+template<class T> void releaseTypeInfo()
+{
+  FFaSingelton<FFlTypeInfoSpec,T>::removeInstance();
+}
+
+
 void FFl::releaseAllElements()
 {
-  FFaSingelton<FFlTypeInfoSpec,FFlNode>::removeInstance();
-
+  releaseTypeInfo<FFlNode>();
   releaseElement<FFlRGD>();
   releaseElement<FFlRBAR>();
   releaseElement<FFlWAVGM>();
@@ -145,43 +150,43 @@ void FFl::releaseAllElements()
   releaseAttribute<FFlSURFLOAD>();
   LoadFactory::removeInstance();
 
-  releaseAttribute<FFlPMASS>();
-  releaseAttribute<FFlPSPRING>();
-  releaseAttribute<FFlPBUSHCOEFF>();
-  releaseAttribute<FFlPBUSHECCENT>();
-  releaseAttribute<FFlPORIENT>();
-  releaseAttribute<FFlPORIENT3>();
-  releaseAttribute<FFlPBEAMECCENT>();
-  releaseAttribute<FFlPBEAMSECTION>();
-  releaseAttribute<FFlPBEAMPIN>();
-  releaseAttribute<FFlPEFFLENGTH>();
-  releaseAttribute<FFlPSPRING>();
-  releaseAttribute<FFlPTHICK>();
-  releaseAttribute<FFlPCOMP>();
-  releaseAttribute<FFlPNSM>();
-  releaseAttribute<FFlPRGD>();
-  releaseAttribute<FFlPWAVGM>();
-  releaseAttribute<FFlPRBAR>();
-  releaseAttribute<FFlPMAT>();
-  releaseAttribute<FFlPMAT2D>();
-  releaseAttribute<FFlPMAT3D>();
-  releaseAttribute<FFlPMATSHELL>();
-  releaseAttribute<FFlPCOORDSYS>();
+  releaseTypeInfo<FFlPMASS>();
+  releaseTypeInfo<FFlPSPRING>();
+  releaseTypeInfo<FFlPBUSHCOEFF>();
+  releaseTypeInfo<FFlPBUSHECCENT>();
+  releaseTypeInfo<FFlPORIENT>();
+  releaseTypeInfo<FFlPORIENT3>();
+  releaseTypeInfo<FFlPBEAMECCENT>();
+  releaseTypeInfo<FFlPBEAMSECTION>();
+  releaseTypeInfo<FFlPBEAMPIN>();
+  releaseTypeInfo<FFlPEFFLENGTH>();
+  releaseTypeInfo<FFlPSPRING>();
+  releaseTypeInfo<FFlPTHICK>();
+  releaseTypeInfo<FFlPCOMP>();
+  releaseTypeInfo<FFlPNSM>();
+  releaseTypeInfo<FFlPRGD>();
+  releaseTypeInfo<FFlPWAVGM>();
+  releaseTypeInfo<FFlPRBAR>();
+  releaseTypeInfo<FFlPMAT>();
+  releaseTypeInfo<FFlPMAT2D>();
+  releaseTypeInfo<FFlPMAT3D>();
+  releaseTypeInfo<FFlPMATSHELL>();
+  releaseTypeInfo<FFlPCOORDSYS>();
 #ifdef FT_USE_STRAINCOAT
   releaseAttribute<FFlPSTRC>();
-  releaseAttribute<FFlPHEIGHT>();
   releaseAttribute<FFlPTHICKREF>();
-  releaseAttribute<FFlPFATIGUE>();
+  releaseTypeInfo<FFlPHEIGHT>();
+  releaseTypeInfo<FFlPFATIGUE>();
 #endif
   AttributeFactory::removeInstance();
 
 #ifdef FT_USE_VISUALS
-  FFlVAppearanceTypeInfoSpec::removeInstance();
-  FFlVDetailTypeInfoSpec::removeInstance();
+  releaseTypeInfo<FFlVAppearance>();
+  releaseTypeInfo<FFlVDetail>();
   VisualFactory::removeInstance();
 #endif
 
-  FFaSingelton<FFlTypeInfoSpec,FFlGroup>::removeInstance();
+  releaseTypeInfo<FFlGroup>();
 
   FFlMemPool::deleteAllLinkMemPools();
 