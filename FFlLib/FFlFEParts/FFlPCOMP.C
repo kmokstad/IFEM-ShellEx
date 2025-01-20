kmokstad/fedem-foundation

Replace class FFlPCOMP::Ply by struct FFlPly to avoid template/namespace troubles

src/FFlLib/FFlFEParts/FFlPCOMP.C
@@ -8,16 +8,10 @@
 #include "FFlLib/FFlFEParts/FFlPCOMP.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 
+#ifdef FT_KERNEL
+namespace FTK {
+#endif
 
-template<> void FFaCheckSum::add(const FFlPCOMP::PlyVec& v)
-{
-  for (const FFlPCOMP::Ply& ply : v)
-  {
-    this->add(ply.MID);
-    this->add(ply.T);
-    this->add(ply.thetaInDeg);
-  }
-}
 
 FFlPCOMP::FFlPCOMP(int id) : FFlAttributeBase(id)
 {
@@ -41,21 +35,24 @@ void FFlPCOMP::convertUnits(const FFaUnitCalculator* convCal)
   // Round to 10 significant digits
   convCal->convert(Z0.data(),"LENGTH",10);
 
-  for (Ply& ply : plySet.data())
+  for (FFlPly& ply : plySet.data())
     convCal->convert(ply.T,"LENGTH",10);
 }
 
 
 void FFlPCOMP::init()
 {
-  FFlPCOMPTypeInfoSpec::instance()->setTypeName("PCOMP");
-  FFlPCOMPTypeInfoSpec::instance()->setDescription("Composite Shell properties");
-  FFlPCOMPTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::GEOMETRY_PROP);
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPCOMP>;
+
+  TypeInfoSpec::instance()->setTypeName("PCOMP");
+  TypeInfoSpec::instance()->setDescription("Composite Shell properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::GEOMETRY_PROP);
 
   AttributeFactory::instance()->registerCreator
-    (FFlPCOMPTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlPCOMP::create,int,FFlAttributeBase*&));
 }
+
 
 void FFlPCOMP::calculateChecksum(FFaCheckSum* cs, int csMask) const
 {
@@ -67,22 +64,26 @@ void FFlPCOMP::calculateChecksum(FFaCheckSum* cs, int csMask) const
   return;
 }
 
+#ifdef FT_KERNEL
+} // namespace FTK
+#endif
 
-//Explicit instantiation
-template FFlField<FFlPCOMP::PlyVec>::FFlField();
+
+// Explicit instantiation
+template FFlField<FFlPlyVec>::FFlField();
 
 template<> inline bool
-FFlField<FFlPCOMP::PlyVec>::parse(std::vector<std::string>::const_iterator& begin,
-				  const std::vector<std::string>::const_iterator& end)
+FFlField<FFlPlyVec>::parse(std::vector<std::string>::const_iterator& begin,
+                           const std::vector<std::string>::const_iterator& end)
 {
   while (begin != end)
   {
-    FFlPCOMP::Ply ply;
+    FFlPly ply;
     if (!parseNumericField(ply.MID,*begin++))   return false;
     if ( begin == end ) return false;
     if (!parseNumericField(ply.T,*begin++))     return false;
     if ( begin == end ) return false;
-    if (!parseNumericField(ply.thetaInDeg,*begin++)) return false;
+    if (!parseNumericField(ply.theta,*begin++)) return false;
 
     myData.push_back(ply);
   }
@@ -91,16 +92,28 @@ FFlField<FFlPCOMP::PlyVec>::parse(std::vector<std::string>::const_iterator& begi
 }
 
 template<> inline void
-FFlField<FFlPCOMP::PlyVec>::write(std::ostream& os) const
+FFlField<FFlPlyVec>::write(std::ostream& os) const
 {
-  for (const FFlPCOMP::Ply& ply : myData)
-    os <<"   "<< ply.MID <<" "<< ply.T <<" "<< ply.thetaInDeg;
+  for (const FFlPly& ply : myData)
+    os <<"   "<< ply.MID <<" "<< ply.T <<" "<< ply.theta;
 }
 
-std::ostream& operator<< (std::ostream& os, const FFlPCOMP::PlyVec& val)
+
+std::ostream& operator<<(std::ostream& os, const FFlPlyVec& val)
 {
-  for (const FFlPCOMP::Ply& ply : val)
-    os <<"\n"<< ply.MID <<" "<< ply.T <<" "<< ply.thetaInDeg;
+  for (const FFlPly& ply : val)
+    os <<"\n"<< ply.MID <<" "<< ply.T <<" "<< ply.theta;
 
   return os;
+}
+
+
+template<> void FFaCheckSum::add(const FFlPlyVec& val)
+{
+  for (const FFlPly& ply : val)
+  {
+    this->add(ply.MID);
+    this->add(ply.T);
+    this->add(ply.theta);
+  }
 }

kmokstad/IFEM-ShellEx

FFlLib/FFlFEParts/FFlPCOMP.C
@@ -8,16 +8,10 @@
 #include "FFlLib/FFlFEParts/FFlPCOMP.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 
+#ifdef FF_NAMESPACE
+namespace FF_NAMESPACE {
+#endif
 
-template<> void FFaCheckSum::add(const FFlPCOMP::PlyVec& v)
-{
-  for (const FFlPCOMP::Ply& ply : v)
-  {
-    this->add(ply.MID);
-    this->add(ply.T);
-    this->add(ply.thetaInDeg);
-  }
-}
 
 FFlPCOMP::FFlPCOMP(int id) : FFlAttributeBase(id)
 {
@@ -41,21 +35,24 @@ void FFlPCOMP::convertUnits(const FFaUnitCalculator* convCal)
   // Round to 10 significant digits
   convCal->convert(Z0.data(),"LENGTH",10);
 
-  for (Ply& ply : plySet.data())
+  for (FFlPly& ply : plySet.data())
     convCal->convert(ply.T,"LENGTH",10);
 }
 
 
 void FFlPCOMP::init()
 {
-  FFlPCOMPTypeInfoSpec::instance()->setTypeName("PCOMP");
-  FFlPCOMPTypeInfoSpec::instance()->setDescription("Composite Shell properties");
-  FFlPCOMPTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::GEOMETRY_PROP);
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPCOMP>;
+
+  TypeInfoSpec::instance()->setTypeName("PCOMP");
+  TypeInfoSpec::instance()->setDescription("Composite Shell properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::GEOMETRY_PROP);
 
   AttributeFactory::instance()->registerCreator
-    (FFlPCOMPTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlPCOMP::create,int,FFlAttributeBase*&));
 }
+
 
 void FFlPCOMP::calculateChecksum(FFaCheckSum* cs, int csMask) const
 {
@@ -67,22 +64,26 @@ void FFlPCOMP::calculateChecksum(FFaCheckSum* cs, int csMask) const
   return;
 }
 
+#ifdef FF_NAMESPACE
+} // namespace
+#endif
 
-//Explicit instantiation
-template FFlField<FFlPCOMP::PlyVec>::FFlField();
+
+// Explicit instantiation
+template FFlField<FFlPlyVec>::FFlField();
 
 template<> inline bool
-FFlField<FFlPCOMP::PlyVec>::parse(std::vector<std::string>::const_iterator& begin,
-				  const std::vector<std::string>::const_iterator& end)
+FFlField<FFlPlyVec>::parse(std::vector<std::string>::const_iterator& begin,
+                           const std::vector<std::string>::const_iterator& end)
 {
   while (begin != end)
   {
-    FFlPCOMP::Ply ply;
+    FFlPly ply;
     if (!parseNumericField(ply.MID,*begin++))   return false;
     if ( begin == end ) return false;
     if (!parseNumericField(ply.T,*begin++))     return false;
     if ( begin == end ) return false;
-    if (!parseNumericField(ply.thetaInDeg,*begin++)) return false;
+    if (!parseNumericField(ply.theta,*begin++)) return false;
 
     myData.push_back(ply);
   }
@@ -91,16 +92,28 @@ FFlField<FFlPCOMP::PlyVec>::parse(std::vector<std::string>::const_iterator& begi
 }
 
 template<> inline void
-FFlField<FFlPCOMP::PlyVec>::write(std::ostream& os) const
+FFlField<FFlPlyVec>::write(std::ostream& os) const
 {
-  for (const FFlPCOMP::Ply& ply : myData)
-    os <<"   "<< ply.MID <<" "<< ply.T <<" "<< ply.thetaInDeg;
+  for (const FFlPly& ply : myData)
+    os <<"   "<< ply.MID <<" "<< ply.T <<" "<< ply.theta;
 }
 
-std::ostream& operator<< (std::ostream& os, const FFlPCOMP::PlyVec& val)
+
+std::ostream& operator<<(std::ostream& os, const FFlPlyVec& val)
 {
-  for (const FFlPCOMP::Ply& ply : val)
-    os <<"\n"<< ply.MID <<" "<< ply.T <<" "<< ply.thetaInDeg;
+  for (const FFlPly& ply : val)
+    os <<"\n"<< ply.MID <<" "<< ply.T <<" "<< ply.theta;
 
   return os;
+}
+
+
+template<> void FFaCheckSum::add(const FFlPlyVec& val)
+{
+  for (const FFlPly& ply : val)
+  {
+    this->add(ply.MID);
+    this->add(ply.T);
+    this->add(ply.theta);
+  }
 }