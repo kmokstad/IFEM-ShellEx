kmokstad/fedem-foundation

Reduce the number of compilation units by adding source files FFlBeams, FFlShells, FFlSolids and FFlLoads containing elements of similar kind

src/FFlLib/FFlFEParts/FFlLoads.C
@@ -5,10 +5,151 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "FFlPLOAD.H"
+#include "FFlLib/FFlFEParts/FFlLoads.H"
 #include "FFlLib/FFlFEParts/FFlPORIENT.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 #include "FFaLib/FFaDefinitions/FFaMsg.H"
+
+#ifdef FT_KERNEL
+namespace FTK {
+#endif
+
+
+FFlCLOAD::FFlCLOAD(int id, const char type) : FFlLoadBase(id), myType(type)
+{
+  this->addField(P);
+
+  target_counter = 0;
+}
+
+
+FFlCLOAD::FFlCLOAD(const FFlCLOAD& obj) : FFlLoadBase(obj), myType(obj.myType)
+{
+  this->addField(P);
+
+  P = obj.P;
+  target_counter = 0;
+  target.resize(obj.target.size());
+  for (size_t i = 0; i < target.size(); i++)
+    target[i] = obj.target[i].getID();
+}
+
+
+bool FFlCLOAD::resolveNodeRef(const std::vector<FFlNode*>& possibleNodes,
+			      bool suppressErrmsg)
+{
+  if (possibleNodes.empty())
+    return false;
+
+  bool retVar = true;
+  for (size_t i = 0; i < target.size(); i++)
+    if (!target[i].resolve(possibleNodes))
+    {
+      if (!suppressErrmsg)
+	ListUI <<"\n *** Error: Failed to resolve reference to node "
+	       << target[i].getID() <<"\n";
+      retVar = false;
+    }
+
+  return retVar;
+}
+
+
+void FFlCLOAD::setTarget(int nodeId, int)
+{
+  target.resize(target.size()+1,nodeId);
+}
+
+
+void FFlCLOAD::setTarget(const std::vector<int>& nodIds)
+{
+  size_t n = target.size();
+  target.resize(n+nodIds.size());
+  for (size_t i = 0; i < nodIds.size(); i++)
+    target[n+i] = nodIds[i];
+}
+
+
+bool FFlCLOAD::getTarget(int& nodID, int& type) const
+{
+  if (type == 0) target_counter = 0;
+
+  if (target_counter >= target.size())
+  {
+    target_counter = 0;
+    return false;
+  }
+
+  nodID = target[target_counter++].getID();
+  type = myType == 'F' ? -1 : -2;
+  return true;
+}
+
+
+int FFlCLOAD::getLoad(std::vector<FaVec3>& F, int& type) const
+{
+  if (type == 0) target_counter = 0;
+
+  if (target_counter >= target.size())
+  {
+    target_counter = 0;
+    return 0;
+  }
+
+  F.clear();
+  F.push_back(P.getValue());
+  type = myType == 'F' ? -1 : -2;
+
+  return target[target_counter++].getID();
+}
+
+
+void FFlCLOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
+{
+  FFlLoadBase::calculateChecksum(cs,csMask);
+
+  cs->add(myType);
+  for (size_t i = 0; i < target.size(); i++)
+    cs->add(target[i].getID());
+}
+
+
+void FFlCFORCE::convertUnits(const FFaUnitCalculator* convCal)
+{
+  convCal->convert(P.data(),"FORCE");
+}
+
+
+void FFlCFORCE::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlCFORCE>;
+
+  TypeInfoSpec::instance()->setTypeName("CFORCE");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+
+  LoadFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlCFORCE::create,int,FFlLoadBase*&));
+}
+
+
+void FFlCMOMENT::convertUnits(const FFaUnitCalculator* convCal)
+{
+  convCal->convert(P.data(),"FORCE/LENGTH");
+}
+
+
+void FFlCMOMENT::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlCMOMENT>;
+
+  TypeInfoSpec::instance()->setTypeName("CMOMENT");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+
+  LoadFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlCMOMENT::create,int,FFlLoadBase*&));
+}
 
 
 FFlPLOAD::FFlPLOAD(int id) : FFlLoadBase(id)
@@ -174,13 +315,15 @@ void FFlSURFLOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
 
 void FFlSURFLOAD::init()
 {
-  FFlSURFLOADTypeInfoSpec::instance()->setTypeName("SURFLOAD");
-  FFlSURFLOADTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  using TypeInfoSpec  = FFaSingelton<FFlTypeInfoSpec,FFlSURFLOAD>;
+  using AttributeSpec = FFaSingelton<FFlFEAttributeSpec,FFlSURFLOAD>;
 
-  FFlSURFLOADAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
+  TypeInfoSpec::instance()->setTypeName("SURFLOAD");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
 
   LoadFactory::instance()->registerCreator
-    (FFlSURFLOADTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlSURFLOAD::create,int,FFlLoadBase*&));
 }
 
@@ -294,12 +437,18 @@ void FFlFACELOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
 
 void FFlFACELOAD::init()
 {
-  FFlFACELOADTypeInfoSpec::instance()->setTypeName("FACELOAD");
-  FFlFACELOADTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  using TypeInfoSpec  = FFaSingelton<FFlTypeInfoSpec,FFlFACELOAD>;
+  using AttributeSpec = FFaSingelton<FFlFEAttributeSpec,FFlFACELOAD>;
 
-  FFlFACELOADAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
+  TypeInfoSpec::instance()->setTypeName("FACELOAD");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
 
   LoadFactory::instance()->registerCreator
-    (FFlFACELOADTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlFACELOAD::create,int,FFlLoadBase*&));
 }
+
+#ifdef FT_KERNEL
+} // namespace FTK
+#endif

src/FFlLib/FFlFEParts/FFlPMAT.C
@@ -8,6 +8,10 @@
 #include "FFlLib/FFlFEParts/FFlPMAT.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 
+#ifdef FT_KERNEL
+namespace FTK {
+#endif
+
 
 FFlPMAT::FFlPMAT(int id) : FFlAttributeBase(id)
 {
@@ -49,11 +53,210 @@ void FFlPMAT::convertUnits(const FFaUnitCalculator* convCal)
 
 void FFlPMAT::init()
 {
-  FFlPMATTypeInfoSpec::instance()->setTypeName("PMAT");
-  FFlPMATTypeInfoSpec::instance()->setDescription("Material properties");
-  FFlPMATTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
 
   AttributeFactory::instance()->registerCreator
-    (FFlPMATTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlPMAT::create,int,FFlAttributeBase*&));
 }
+
+
+FFlPMAT2D::FFlPMAT2D(int id) : FFlAttributeBase(id)
+{
+  for (FFlField<double>& field : C)
+    this->addField(field);
+
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E  = 205.0e9;
+  double nu = 0.29;
+  double f0 = E / (1.0-nu*nu);
+
+  C[0].setValue(f0);            // C(1,1)
+  C[1].setValue(f0*nu);         // C(1,2)
+  C[3].setValue(f0);            // C(2,2)
+  C[5].setValue(f0*(1-nu)*0.5); // C(3,3)
+
+  materialDensity.setValue(7850.0);
+}
+
+
+FFlPMAT2D::FFlPMAT2D(const FFlPMAT2D& obj) : FFlAttributeBase(obj)
+{
+  for (size_t i = 0; i < C.size(); i++)
+  {
+    this->addField(C[i]);
+    C[i].setValue(obj.C[i].getValue());
+  }
+
+  this->addField(materialDensity);
+  materialDensity.setValue(obj.materialDensity.getValue());
+}
+
+
+void FFlPMAT2D::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  for (FFlField<double>& field : C)
+    convCal->convert(field.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMAT2D::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT2D>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT2D");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMAT2D::create,int,FFlAttributeBase*&));
+}
+
+
+FFlPMAT3D::FFlPMAT3D(int id) : FFlAttributeBase(id)
+{
+  for (FFlField<double>& field : C)
+    this->addField(field);
+
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E  = 205.0e9;
+  double nu = 0.29;
+  double f1 = E * (1.0-nu) / ((1.0+nu)*(1.0-2.0*nu));
+  double f2 = f1 * nu / (1.0-nu);
+  double f3 = f1 * (0.5-nu);
+
+  C[ 0].setValue(f1); // C(1,1)
+  C[ 1].setValue(f2); // C(1,2)
+  C[ 2].setValue(f2); // C(1,3)
+  C[ 6].setValue(f1); // C(2,2)
+  C[ 7].setValue(f2); // C(2,3)
+  C[11].setValue(f1); // C(3,3)
+  C[15].setValue(f3); // C(4,4)
+  C[18].setValue(f3); // C(5,5)
+  C[20].setValue(f3); // C(6,6)
+
+  materialDensity.setValue(7850.0);
+}
+
+
+FFlPMAT3D::FFlPMAT3D(const FFlPMAT3D& obj) : FFlAttributeBase(obj)
+{
+  for (size_t i = 0; i < C.size(); i++)
+  {
+    this->addField(C[i]);
+    C[i].setValue(obj.C[i].getValue());
+  }
+
+  this->addField(materialDensity);
+  materialDensity.setValue(obj.materialDensity.getValue());
+}
+
+
+void FFlPMAT3D::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  for (FFlField<double>& field : C)
+    convCal->convert(field.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMAT3D::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT3D>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT3D");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMAT3D::create,int,FFlAttributeBase*&));
+}
+
+
+FFlPMATSHELL::FFlPMATSHELL(int id) : FFlAttributeBase(id)
+{
+  this->addField(E1);
+  this->addField(E2);
+  this->addField(NU12);
+  this->addField(G12);
+  this->addField(G1Z);
+  this->addField(G2Z);
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E            = 205.00E+9;
+  double shearModule  =  80.00E+9;
+  double nu           =   0.29E+0;
+
+  E1 = E;
+  E2 = E;
+  NU12 = nu;
+  G12 = shearModule;
+  G1Z = shearModule;
+  G2Z = shearModule;
+  materialDensity = 7.85E+3;
+}
+
+
+FFlPMATSHELL::FFlPMATSHELL(const FFlPMATSHELL& obj) : FFlAttributeBase(obj)
+{
+  this->addField(E1);
+  this->addField(E2);
+  this->addField(NU12);
+  this->addField(G12);
+  this->addField(G1Z);
+  this->addField(G2Z);
+  this->addField(materialDensity);
+
+  E1 = obj.E1;
+  E2 = obj.E2;
+  NU12 = obj.NU12;
+  G12 = obj.G12;
+  G1Z = obj.G1Z;
+  G2Z = obj.G2Z;
+  materialDensity = obj.materialDensity;
+}
+
+
+void FFlPMATSHELL::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  convCal->convert(E1.data(),"FORCE/AREA",10);
+  convCal->convert(E2.data(),"FORCE/AREA",10);
+  convCal->convert(NU12.data(),"FORCE/AREA",10);
+  convCal->convert(G12.data(),"FORCE/AREA",10);
+  convCal->convert(G1Z.data(),"FORCE/AREA",10);
+  convCal->convert(G2Z.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMATSHELL::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMATSHELL>;
+
+  TypeInfoSpec::instance()->setTypeName("PMATSHELL");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMATSHELL::create,int,FFlAttributeBase*&));
+}
+
+#ifdef FT_KERNEL
+} // namespace FTK
+#endif

src/FFlLib/FFlMemPool.C
@@ -14,18 +14,9 @@
 #include "FFlLib/FFlFEParts/FFlCMASS.H"
 #include "FFlLib/FFlFEParts/FFlSPRING.H"
 #include "FFlLib/FFlFEParts/FFlBUSH.H"
-#include "FFlLib/FFlFEParts/FFlBEAM2.H"
-#include "FFlLib/FFlFEParts/FFlBEAM3.H"
-#include "FFlLib/FFlFEParts/FFlTRI3.H"
-#include "FFlLib/FFlFEParts/FFlTRI6.H"
-#include "FFlLib/FFlFEParts/FFlQUAD4.H"
-#include "FFlLib/FFlFEParts/FFlQUAD8.H"
-#include "FFlLib/FFlFEParts/FFlTET4.H"
-#include "FFlLib/FFlFEParts/FFlTET10.H"
-#include "FFlLib/FFlFEParts/FFlWEDG6.H"
-#include "FFlLib/FFlFEParts/FFlWEDG15.H"
-#include "FFlLib/FFlFEParts/FFlHEX8.H"
-#include "FFlLib/FFlFEParts/FFlHEX20.H"
+#include "FFlLib/FFlFEParts/FFlBeams.H"
+#include "FFlLib/FFlFEParts/FFlShells.H"
+#include "FFlLib/FFlFEParts/FFlSolids.H"
 #include "FFlLib/FFlFEParts/FFlRBAR.H"
 #include "FFlLib/FFlFEParts/FFlRGD.H"
 #include "FFlLib/FFlFEParts/FFlWAVGM.H"
@@ -33,7 +24,10 @@
 #include "FFlLib/FFlFEParts/FFlSTRCoat.H"
 #endif
 
-typedef FFaSingelton<FFaMemPoolMgr,FFlElementBase> FFlMemPoolMgr;
+#ifdef FT_KERNEL
+using namespace FTK;
+#endif
+using FFlMemPoolMgr = FFaSingelton<FFaMemPoolMgr,FFlElementBase>;
 
 FFaMemPool FFlNode   ::ourMemPool(sizeof(FFlNode));
 #ifdef FT_USE_VERTEX

kmokstad/IFEM-ShellEx

FFlLib/FFlFEParts/FFlLoads.C
@@ -5,10 +5,151 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "FFlPLOAD.H"
+#include "FFlLib/FFlFEParts/FFlLoads.H"
 #include "FFlLib/FFlFEParts/FFlPORIENT.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 #include "FFaLib/FFaDefinitions/FFaMsg.H"
+
+#ifdef FF_NAMESPACE
+namespace FF_NAMESPACE {
+#endif
+
+
+FFlCLOAD::FFlCLOAD(int id, const char type) : FFlLoadBase(id), myType(type)
+{
+  this->addField(P);
+
+  target_counter = 0;
+}
+
+
+FFlCLOAD::FFlCLOAD(const FFlCLOAD& obj) : FFlLoadBase(obj), myType(obj.myType)
+{
+  this->addField(P);
+
+  P = obj.P;
+  target_counter = 0;
+  target.resize(obj.target.size());
+  for (size_t i = 0; i < target.size(); i++)
+    target[i] = obj.target[i].getID();
+}
+
+
+bool FFlCLOAD::resolveNodeRef(const std::vector<FFlNode*>& possibleNodes,
+			      bool suppressErrmsg)
+{
+  if (possibleNodes.empty())
+    return false;
+
+  bool retVar = true;
+  for (size_t i = 0; i < target.size(); i++)
+    if (!target[i].resolve(possibleNodes))
+    {
+      if (!suppressErrmsg)
+	ListUI <<"\n *** Error: Failed to resolve reference to node "
+	       << target[i].getID() <<"\n";
+      retVar = false;
+    }
+
+  return retVar;
+}
+
+
+void FFlCLOAD::setTarget(int nodeId, int)
+{
+  target.resize(target.size()+1,nodeId);
+}
+
+
+void FFlCLOAD::setTarget(const std::vector<int>& nodIds)
+{
+  size_t n = target.size();
+  target.resize(n+nodIds.size());
+  for (size_t i = 0; i < nodIds.size(); i++)
+    target[n+i] = nodIds[i];
+}
+
+
+bool FFlCLOAD::getTarget(int& nodID, int& type) const
+{
+  if (type == 0) target_counter = 0;
+
+  if (target_counter >= target.size())
+  {
+    target_counter = 0;
+    return false;
+  }
+
+  nodID = target[target_counter++].getID();
+  type = myType == 'F' ? -1 : -2;
+  return true;
+}
+
+
+int FFlCLOAD::getLoad(std::vector<FaVec3>& F, int& type) const
+{
+  if (type == 0) target_counter = 0;
+
+  if (target_counter >= target.size())
+  {
+    target_counter = 0;
+    return 0;
+  }
+
+  F.clear();
+  F.push_back(P.getValue());
+  type = myType == 'F' ? -1 : -2;
+
+  return target[target_counter++].getID();
+}
+
+
+void FFlCLOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
+{
+  FFlLoadBase::calculateChecksum(cs,csMask);
+
+  cs->add(myType);
+  for (size_t i = 0; i < target.size(); i++)
+    cs->add(target[i].getID());
+}
+
+
+void FFlCFORCE::convertUnits(const FFaUnitCalculator* convCal)
+{
+  convCal->convert(P.data(),"FORCE");
+}
+
+
+void FFlCFORCE::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlCFORCE>;
+
+  TypeInfoSpec::instance()->setTypeName("CFORCE");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+
+  LoadFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlCFORCE::create,int,FFlLoadBase*&));
+}
+
+
+void FFlCMOMENT::convertUnits(const FFaUnitCalculator* convCal)
+{
+  convCal->convert(P.data(),"FORCE/LENGTH");
+}
+
+
+void FFlCMOMENT::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlCMOMENT>;
+
+  TypeInfoSpec::instance()->setTypeName("CMOMENT");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+
+  LoadFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlCMOMENT::create,int,FFlLoadBase*&));
+}
 
 
 FFlPLOAD::FFlPLOAD(int id) : FFlLoadBase(id)
@@ -174,13 +315,15 @@ void FFlSURFLOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
 
 void FFlSURFLOAD::init()
 {
-  FFlSURFLOADTypeInfoSpec::instance()->setTypeName("SURFLOAD");
-  FFlSURFLOADTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  using TypeInfoSpec  = FFaSingelton<FFlTypeInfoSpec,FFlSURFLOAD>;
+  using AttributeSpec = FFaSingelton<FFlFEAttributeSpec,FFlSURFLOAD>;
 
-  FFlSURFLOADAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
+  TypeInfoSpec::instance()->setTypeName("SURFLOAD");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
 
   LoadFactory::instance()->registerCreator
-    (FFlSURFLOADTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlSURFLOAD::create,int,FFlLoadBase*&));
 }
 
@@ -294,12 +437,18 @@ void FFlFACELOAD::calculateChecksum(FFaCheckSum* cs, int csMask) const
 
 void FFlFACELOAD::init()
 {
-  FFlFACELOADTypeInfoSpec::instance()->setTypeName("FACELOAD");
-  FFlFACELOADTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  using TypeInfoSpec  = FFaSingelton<FFlTypeInfoSpec,FFlFACELOAD>;
+  using AttributeSpec = FFaSingelton<FFlFEAttributeSpec,FFlFACELOAD>;
 
-  FFlFACELOADAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
+  TypeInfoSpec::instance()->setTypeName("FACELOAD");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::LOAD);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
 
   LoadFactory::instance()->registerCreator
-    (FFlFACELOADTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlFACELOAD::create,int,FFlLoadBase*&));
 }
+
+#ifdef FF_NAMESPACE
+} // namespace
+#endif

FFlLib/FFlFEParts/FFlPMAT.C
@@ -8,6 +8,10 @@
 #include "FFlLib/FFlFEParts/FFlPMAT.H"
 #include "FFaLib/FFaAlgebra/FFaUnitCalculator.H"
 
+#ifdef FF_NAMESPACE
+namespace FF_NAMESPACE {
+#endif
+
 
 FFlPMAT::FFlPMAT(int id) : FFlAttributeBase(id)
 {
@@ -49,11 +53,210 @@ void FFlPMAT::convertUnits(const FFaUnitCalculator* convCal)
 
 void FFlPMAT::init()
 {
-  FFlPMATTypeInfoSpec::instance()->setTypeName("PMAT");
-  FFlPMATTypeInfoSpec::instance()->setDescription("Material properties");
-  FFlPMATTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
 
   AttributeFactory::instance()->registerCreator
-    (FFlPMATTypeInfoSpec::instance()->getTypeName(),
+    (TypeInfoSpec::instance()->getTypeName(),
      FFaDynCB2S(FFlPMAT::create,int,FFlAttributeBase*&));
 }
+
+
+FFlPMAT2D::FFlPMAT2D(int id) : FFlAttributeBase(id)
+{
+  for (FFlField<double>& field : C)
+    this->addField(field);
+
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E  = 205.0e9;
+  double nu = 0.29;
+  double f0 = E / (1.0-nu*nu);
+
+  C[0].setValue(f0);            // C(1,1)
+  C[1].setValue(f0*nu);         // C(1,2)
+  C[3].setValue(f0);            // C(2,2)
+  C[5].setValue(f0*(1-nu)*0.5); // C(3,3)
+
+  materialDensity.setValue(7850.0);
+}
+
+
+FFlPMAT2D::FFlPMAT2D(const FFlPMAT2D& obj) : FFlAttributeBase(obj)
+{
+  for (size_t i = 0; i < C.size(); i++)
+  {
+    this->addField(C[i]);
+    C[i].setValue(obj.C[i].getValue());
+  }
+
+  this->addField(materialDensity);
+  materialDensity.setValue(obj.materialDensity.getValue());
+}
+
+
+void FFlPMAT2D::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  for (FFlField<double>& field : C)
+    convCal->convert(field.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMAT2D::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT2D>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT2D");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMAT2D::create,int,FFlAttributeBase*&));
+}
+
+
+FFlPMAT3D::FFlPMAT3D(int id) : FFlAttributeBase(id)
+{
+  for (FFlField<double>& field : C)
+    this->addField(field);
+
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E  = 205.0e9;
+  double nu = 0.29;
+  double f1 = E * (1.0-nu) / ((1.0+nu)*(1.0-2.0*nu));
+  double f2 = f1 * nu / (1.0-nu);
+  double f3 = f1 * (0.5-nu);
+
+  C[ 0].setValue(f1); // C(1,1)
+  C[ 1].setValue(f2); // C(1,2)
+  C[ 2].setValue(f2); // C(1,3)
+  C[ 6].setValue(f1); // C(2,2)
+  C[ 7].setValue(f2); // C(2,3)
+  C[11].setValue(f1); // C(3,3)
+  C[15].setValue(f3); // C(4,4)
+  C[18].setValue(f3); // C(5,5)
+  C[20].setValue(f3); // C(6,6)
+
+  materialDensity.setValue(7850.0);
+}
+
+
+FFlPMAT3D::FFlPMAT3D(const FFlPMAT3D& obj) : FFlAttributeBase(obj)
+{
+  for (size_t i = 0; i < C.size(); i++)
+  {
+    this->addField(C[i]);
+    C[i].setValue(obj.C[i].getValue());
+  }
+
+  this->addField(materialDensity);
+  materialDensity.setValue(obj.materialDensity.getValue());
+}
+
+
+void FFlPMAT3D::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  for (FFlField<double>& field : C)
+    convCal->convert(field.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMAT3D::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMAT3D>;
+
+  TypeInfoSpec::instance()->setTypeName("PMAT3D");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMAT3D::create,int,FFlAttributeBase*&));
+}
+
+
+FFlPMATSHELL::FFlPMATSHELL(int id) : FFlAttributeBase(id)
+{
+  this->addField(E1);
+  this->addField(E2);
+  this->addField(NU12);
+  this->addField(G12);
+  this->addField(G1Z);
+  this->addField(G2Z);
+  this->addField(materialDensity);
+
+  // default values - Typical for steel
+  double E            = 205.00E+9;
+  double shearModule  =  80.00E+9;
+  double nu           =   0.29E+0;
+
+  E1 = E;
+  E2 = E;
+  NU12 = nu;
+  G12 = shearModule;
+  G1Z = shearModule;
+  G2Z = shearModule;
+  materialDensity = 7.85E+3;
+}
+
+
+FFlPMATSHELL::FFlPMATSHELL(const FFlPMATSHELL& obj) : FFlAttributeBase(obj)
+{
+  this->addField(E1);
+  this->addField(E2);
+  this->addField(NU12);
+  this->addField(G12);
+  this->addField(G1Z);
+  this->addField(G2Z);
+  this->addField(materialDensity);
+
+  E1 = obj.E1;
+  E2 = obj.E2;
+  NU12 = obj.NU12;
+  G12 = obj.G12;
+  G1Z = obj.G1Z;
+  G2Z = obj.G2Z;
+  materialDensity = obj.materialDensity;
+}
+
+
+void FFlPMATSHELL::convertUnits(const FFaUnitCalculator* convCal)
+{
+  // Round to 10 significant digits
+  convCal->convert(E1.data(),"FORCE/AREA",10);
+  convCal->convert(E2.data(),"FORCE/AREA",10);
+  convCal->convert(NU12.data(),"FORCE/AREA",10);
+  convCal->convert(G12.data(),"FORCE/AREA",10);
+  convCal->convert(G1Z.data(),"FORCE/AREA",10);
+  convCal->convert(G2Z.data(),"FORCE/AREA",10);
+  convCal->convert(materialDensity.data(),"MASS/VOLUME",10);
+}
+
+
+void FFlPMATSHELL::init()
+{
+  using TypeInfoSpec = FFaSingelton<FFlTypeInfoSpec,FFlPMATSHELL>;
+
+  TypeInfoSpec::instance()->setTypeName("PMATSHELL");
+  TypeInfoSpec::instance()->setDescription("Material properties");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::MATERIAL_PROP);
+
+  AttributeFactory::instance()->registerCreator
+    (TypeInfoSpec::instance()->getTypeName(),
+     FFaDynCB2S(FFlPMATSHELL::create,int,FFlAttributeBase*&));
+}
+
+#ifdef FF_NAMESPACE
+} // namespace
+#endif

FFlLib/FFlMemPool.C
@@ -14,18 +14,9 @@
 #include "FFlLib/FFlFEParts/FFlCMASS.H"
 #include "FFlLib/FFlFEParts/FFlSPRING.H"
 #include "FFlLib/FFlFEParts/FFlBUSH.H"
-#include "FFlLib/FFlFEParts/FFlBEAM2.H"
-#include "FFlLib/FFlFEParts/FFlBEAM3.H"
-#include "FFlLib/FFlFEParts/FFlTRI3.H"
-#include "FFlLib/FFlFEParts/FFlTRI6.H"
-#include "FFlLib/FFlFEParts/FFlQUAD4.H"
-#include "FFlLib/FFlFEParts/FFlQUAD8.H"
-#include "FFlLib/FFlFEParts/FFlTET4.H"
-#include "FFlLib/FFlFEParts/FFlTET10.H"
-#include "FFlLib/FFlFEParts/FFlWEDG6.H"
-#include "FFlLib/FFlFEParts/FFlWEDG15.H"
-#include "FFlLib/FFlFEParts/FFlHEX8.H"
-#include "FFlLib/FFlFEParts/FFlHEX20.H"
+#include "FFlLib/FFlFEParts/FFlBeams.H"
+#include "FFlLib/FFlFEParts/FFlShells.H"
+#include "FFlLib/FFlFEParts/FFlSolids.H"
 #include "FFlLib/FFlFEParts/FFlRBAR.H"
 #include "FFlLib/FFlFEParts/FFlRGD.H"
 #include "FFlLib/FFlFEParts/FFlWAVGM.H"
@@ -33,7 +24,10 @@
 #include "FFlLib/FFlFEParts/FFlSTRCoat.H"
 #endif
 
-typedef FFaSingelton<FFaMemPoolMgr,FFlElementBase> FFlMemPoolMgr;
+#ifdef FF_NAMESPACE
+using namespace FF_NAMESPACE;
+#endif
+using FFlMemPoolMgr = FFaSingelton<FFaMemPoolMgr,FFlElementBase>;
 
 FFaMemPool FFlNode   ::ourMemPool(sizeof(FFlNode));
 #ifdef FT_USE_VERTEX