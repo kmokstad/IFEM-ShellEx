kmokstad/fedem-foundation

Optionally wrap the FFlLib classes within the FTK namespace to avoid name clash when loading two shared libraries both containing these classes, but with different build options. Replace global-scoped typedef with local aliases in init methods.

src/FFlLib/FFlFEParts/FFlBUSH.C
@@ -8,23 +8,31 @@
 #include "FFlLib/FFlFEParts/FFlBUSH.H"
 #include "FFlLib/FFlFEParts/FFlPBUSHECCENT.H"
 
+#ifdef FT_KERNEL
+namespace FTK {
+#endif
+
 
 void FFlBUSH::init()
 {
-  FFlBUSHTypeInfoSpec::instance()->setTypeName("BUSH");
-  FFlBUSHTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlBUSH>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlBUSH>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlBUSH>;
 
-  ElementFactory::instance()->registerCreator(FFlBUSHTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlBUSH::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("BUSH");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHCOEFF", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHECCENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHORIENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PCOORDSYS", false);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlBUSH::create,int,FFlElementBase*&));
 
-  FFlBUSHElementTopSpec::instance()->setNodeCount(2);
-  FFlBUSHElementTopSpec::instance()->setNodeDOFs(6);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHCOEFF",false);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHECCENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHORIENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PCOORDSYS",false);
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(6);
 }
 
 
@@ -51,3 +59,7 @@ int FFlBUSH::getNodalCoor(double* X, double* Y, double* Z) const
 
   return ierr;
 }
+
+#ifdef FT_KERNEL
+} // namespace FTK
+#endif

src/FFlLib/FFlFEParts/FFlSPRING.C
@@ -7,30 +7,48 @@
 
 #include "FFlLib/FFlFEParts/FFlSPRING.H"
 
+#ifdef FT_KERNEL
+namespace FTK {
+#endif
+
 
 void FFlSPRING::init()
 {
-  FFlSPRINGTypeInfoSpec::instance()->setTypeName("SPRING");
-  FFlSPRINGTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlSPRING>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlSPRING>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlSPRING>;
 
-  ElementFactory::instance()->registerCreator(FFlSPRINGTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlSPRING::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("SPRING");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlSPRINGAttributeSpec::instance()->addLegalAttribute("PSPRING");
-  FFlSPRINGElementTopSpec::instance()->setNodeCount(2);
-  FFlSPRINGElementTopSpec::instance()->setNodeDOFs(3);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlSPRING::create,int,FFlElementBase*&));
+
+  AttributeSpec::instance()->addLegalAttribute("PSPRING");
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(3);
 }
 
 
 void FFlRSPRING::init()
 {
-  FFlRSPRINGTypeInfoSpec::instance()->setTypeName("RSPRING");
-  FFlRSPRINGTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlRSPRING>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlRSPRING>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlRSPRING>;
 
-  ElementFactory::instance()->registerCreator(FFlRSPRINGTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlRSPRING::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("RSPRING");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlRSPRINGAttributeSpec::instance()->addLegalAttribute("PSPRING");
-  FFlRSPRINGElementTopSpec::instance()->setNodeCount(2);
-  FFlRSPRINGElementTopSpec::instance()->setNodeDOFs(6);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlRSPRING::create,int,FFlElementBase*&));
+
+  AttributeSpec::instance()->addLegalAttribute("PSPRING");
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(6);
 }
+
+#ifdef FT_KERNEL
+} // namespace FTK
+#endif

kmokstad/IFEM-ShellEx

FFlLib/FFlFEParts/FFlBUSH.C
@@ -8,23 +8,31 @@
 #include "FFlLib/FFlFEParts/FFlBUSH.H"
 #include "FFlLib/FFlFEParts/FFlPBUSHECCENT.H"
 
+#ifdef FF_NAMESPACE
+namespace FF_NAMESPACE {
+#endif
+
 
 void FFlBUSH::init()
 {
-  FFlBUSHTypeInfoSpec::instance()->setTypeName("BUSH");
-  FFlBUSHTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlBUSH>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlBUSH>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlBUSH>;
 
-  ElementFactory::instance()->registerCreator(FFlBUSHTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlBUSH::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("BUSH");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHCOEFF", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHECCENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PBUSHORIENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PORIENT", false);
-  FFlBUSHAttributeSpec::instance()->addLegalAttribute("PCOORDSYS", false);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlBUSH::create,int,FFlElementBase*&));
 
-  FFlBUSHElementTopSpec::instance()->setNodeCount(2);
-  FFlBUSHElementTopSpec::instance()->setNodeDOFs(6);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHCOEFF",false);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHECCENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PBUSHORIENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PORIENT",false);
+  AttributeSpec::instance()->addLegalAttribute("PCOORDSYS",false);
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(6);
 }
 
 
@@ -51,3 +59,7 @@ int FFlBUSH::getNodalCoor(double* X, double* Y, double* Z) const
 
   return ierr;
 }
+
+#ifdef FF_NAMESPACE
+} // namespace
+#endif

FFlLib/FFlFEParts/FFlSPRING.C
@@ -7,30 +7,48 @@
 
 #include "FFlLib/FFlFEParts/FFlSPRING.H"
 
+#ifdef FF_NAMESPACE
+namespace FF_NAMESPACE {
+#endif
+
 
 void FFlSPRING::init()
 {
-  FFlSPRINGTypeInfoSpec::instance()->setTypeName("SPRING");
-  FFlSPRINGTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlSPRING>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlSPRING>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlSPRING>;
 
-  ElementFactory::instance()->registerCreator(FFlSPRINGTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlSPRING::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("SPRING");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlSPRINGAttributeSpec::instance()->addLegalAttribute("PSPRING");
-  FFlSPRINGElementTopSpec::instance()->setNodeCount(2);
-  FFlSPRINGElementTopSpec::instance()->setNodeDOFs(3);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlSPRING::create,int,FFlElementBase*&));
+
+  AttributeSpec::instance()->addLegalAttribute("PSPRING");
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(3);
 }
 
 
 void FFlRSPRING::init()
 {
-  FFlRSPRINGTypeInfoSpec::instance()->setTypeName("RSPRING");
-  FFlRSPRINGTypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
+  using TypeInfoSpec   = FFaSingelton<FFlTypeInfoSpec,FFlRSPRING>;
+  using AttributeSpec  = FFaSingelton<FFlFEAttributeSpec,FFlRSPRING>;
+  using ElementTopSpec = FFaSingelton<FFlFEElementTopSpec,FFlRSPRING>;
 
-  ElementFactory::instance()->registerCreator(FFlRSPRINGTypeInfoSpec::instance()->getTypeName(),
-					      FFaDynCB2S(FFlRSPRING::create,int,FFlElementBase*&));
+  TypeInfoSpec::instance()->setTypeName("RSPRING");
+  TypeInfoSpec::instance()->setCathegory(FFlTypeInfoSpec::OTHER_ELM);
 
-  FFlRSPRINGAttributeSpec::instance()->addLegalAttribute("PSPRING");
-  FFlRSPRINGElementTopSpec::instance()->setNodeCount(2);
-  FFlRSPRINGElementTopSpec::instance()->setNodeDOFs(6);
+  ElementFactory::instance()->registerCreator(TypeInfoSpec::instance()->getTypeName(),
+                                              FFaDynCB2S(FFlRSPRING::create,int,FFlElementBase*&));
+
+  AttributeSpec::instance()->addLegalAttribute("PSPRING");
+
+  ElementTopSpec::instance()->setNodeCount(2);
+  ElementTopSpec::instance()->setNodeDOFs(6);
 }
+
+#ifdef FF_NAMESPACE
+} // namespace
+#endif